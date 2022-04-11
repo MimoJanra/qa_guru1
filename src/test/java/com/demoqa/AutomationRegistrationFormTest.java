ed0821774c0022acc834828e026d1b4510ddd464
Artem Alekseev <alk@tassta.com>
2022-04-11 10:02:43 +0300

    lesson 4

--- src/test/java/com/demoqa/AutomationRegistrationFormTest.java
+++ src/test/java/com/demoqa/AutomationRegistrationFormTest.java
@@ -1,41 +1,38 @@
 package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
+import pages.RegistrationFormPage;
 
 import java.util.stream.Stream;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class AutomationRegistrationFormTest {
 
-    String studentRegistrationForm = "/automation-practice-form";
-
-    String fistName = "Gogi";
-    String lastName = "Velikolepniy";
-    String email = "gogi@behinavole.pro";
-    String gender = "Other";
-    String mobile = "1234567892";
-    String birthDay = "11";
-    String birthMonth = "February";
-    String birthYear = "1901";
-    String dateOfBirthFormatted = birthMonth + " " + birthDay + "th, " + birthYear;
-    String subjects = "History";
-    String hobbies = "Music";
-    String address = "Khasa Kothi Circle, MI Rd";
-    String state = "NCR";
-    String city = "Gurgaon";
+    Faker faker = new Faker();
+    String studentRegistrationForm = "/automation-practice-form",
+            firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(lastName),
+            gender = faker.demographic().sex(),
+            mobile = faker.numerify("##########"),
+            day = "11",
+            month = "April",
+            year = "1901",
+            subjects = "History",
+            hobbies = "Music",
+            address = faker.address().fullAddress(),
+            state = "NCR",
+            city = "Gurgaon";
 
     @BeforeAll
-    static void BeforeAll(){
+    static void BeforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
     }
 
@@ -55,53 +52,37 @@ public class AutomationRegistrationFormTest {
     @ParameterizedTest
     @MethodSource("browserResolution")
     void fillSubmitCloseRegistrationForm(String resolution) {
+        RegistrationFormPage registrationFormPage = new RegistrationFormPage();
         Configuration.browserSize = resolution;
         open(studentRegistrationForm);
 
-        //close ads
-        $("#close-fixedban").click();
+        registrationFormPage.setFirstName(firstName)
+                .setLastName(lastName)
+                .setUserEmail(email)
+                .setGender(gender)
+                .setUserNumber(mobile)
+                .setBirthDate(day, month, year)
+                .setSubjects(subjects)
+                .setUserHobbies(hobbies)
+                .upLoadPicture("img/1.png")
+                .setAddress(address)
+                .setState(state)
+                .setCity(city)
+                .submit();
 
-        //Filling form
-        $("#firstName").setValue(fistName);
-        $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(email);
-        $(byText(gender)).click();
-        $("#userNumber").setValue(mobile);
-        $("#dateOfBirthInput").scrollIntoView(true).click();
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption(birthMonth);
-        $(".react-datepicker__year-select").selectOption(birthYear);
-        $("[aria-label$='" + dateOfBirthFormatted + "']").click();
-        $("#subjectsInput").setValue(subjects).pressEnter();
-        $(byText("Music")).click();
-        $("#uploadPicture").uploadFromClasspath("img/1.png");
-        $("#currentAddress").setValue(address);
-        $("#state").scrollIntoView(true).click();
-        $(byText(state)).click();
-        $("#city").click();
-        $(byText(city)).click();
-        $("#submit").click();
+        registrationFormPage.checkTableHeaderHasText("Thanks for submitting the form")
+                .checkTableRowHasText("Student Name", firstName)
+                .checkTableRowHasText("Student Email", email)
+                .checkTableRowHasText("Gender", gender)
+                .checkTableRowHasText("Mobile", mobile)
+                .checkTableRowHasText("Date of Birth", day + " " + month + "," + year)
+                .checkTableRowHasText("Subjects", String.join(", ", subjects))
+                .checkTableRowHasText("Hobbies", String.join(", ", hobbies))
+                .checkTableRowHasText("Picture", "1.png")
+                .checkTableRowHasText("Address", address)
+                .checkTableRowHasText("Mobile", mobile)
+                .checkTableRowHasText("State and City", state + " " + city);
 
-        // Check form
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $x(getCell("Student Name")).shouldHave(text(fistName + " " + lastName));
-        $x(getCell("Student Email")).shouldHave(text(email));
-        $x(getCell("Gender")).shouldHave(text(gender));
-        $x(getCell("Mobile")).shouldHave(text(mobile));
-        $x(getCell("Date of Birth")).shouldHave(text(birthDay + " " + birthMonth + "," + birthYear));
-        $x(getCell("Subjects")).shouldHave(text(subjects));
-        $x(getCell("Hobbies")).shouldHave(text(hobbies));
-        $x(getCell("Picture")).shouldHave(text("1.png"));
-        $x(getCell("Address")).shouldHave(text(address));
-        $x(getCell("Mobile")).shouldHave(text(mobile));
-        $x(getCell("State and City")).shouldHave(text(state + " " + city));
-
-        $("#closeLargeModal").click();
-
-        $("#example-modal-sizes-title-lg").shouldNotBe(visible);
-    }
-
-    public static String getCell(String label) {
-        return "//td[preceding-sibling::td='" + label + "']";
+        registrationFormPage.closeModal();
     }
 }
