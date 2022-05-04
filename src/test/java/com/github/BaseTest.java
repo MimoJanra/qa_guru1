e0298d01844cb7e99360e4ecbb314f0e3d327e71
Artem Alekseev <alk@tassta.com>
2022-05-04 16:59:05 +0300

    lesson 8

--- src/test/java/com/github/BaseTest.java
+++ src/test/java/com/github/BaseTest.java
@@ -1,29 +1,75 @@
 package com.github;
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.pdftest.matchers.ContainsExactText;
+import com.opencsv.CSVReader;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byLinkText;
-import static com.codeborne.selenide.Selenide.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BaseTest {
+    ClassLoader classLoader = BaseTest.class.getClassLoader();
+
     @Test
-    void wikisCheckInSelenide() {
-        open("https://github.com/");
-        $(".header-search-input").click();
-        $(".header-search-input").setValue("Selenide").pressEnter();
-        $$(".repo-list").first().$("a").click();
-        $("#wiki-tab").click();
+    void zipParsingPDFTest() throws Exception {
+        ZipFile zf = new ZipFile(new File("src/test/resources/PDF.zip"));
+        try (ZipInputStream is = new ZipInputStream(Objects.requireNonNull(classLoader.getResourceAsStream("PDF.zip")))) {
+            ZipEntry entry;
+            while ((entry = is.getNextEntry()) != null) {
+                org.assertj.core.api.Assertions.assertThat(entry.getName()).isEqualTo("sample.pdf");
+                try (InputStream inputStream = zf.getInputStream(entry)) {
+                    PDF pdf = new PDF(inputStream);
+                    Assertions.assertThat(pdf.numberOfPages).isEqualTo(2);
+                    assertThat(pdf, new ContainsExactText("A Simple PDF File"));
+                }
+            }
+        }
+    }
 
-        $("#wiki-pages-box ul li button").scrollTo().click();
-        $("#wiki-pages-box").$(byLinkText("SoftAssertions")).click();
-        $("#wiki-wrapper h1").shouldHave(text("SoftAssertions"));
-
-        $("#wiki-body h4 a#user-content-3-using-junit5-extend-test-class")
-                .scrollTo()
-                .shouldBe(visible)
-                .ancestor("h4")
-                .shouldHave(text("Junit5"));
+    @Test
+    void zipParsingPngTest() throws Exception {
+        ZipFile zf = new ZipFile(new File("src/test/resources/PNG.zip"));
+        try (ZipInputStream is = new ZipInputStream(Objects.requireNonNull(classLoader.getResourceAsStream("PNG.zip")))) {
+            ZipEntry entry;
+            while ((entry = is.getNextEntry()) != null) {
+                org.assertj.core.api.Assertions.assertThat(entry.getName()).isEqualTo("Sample.png");
+                try (InputStream inputStream = zf.getInputStream(entry)) {
+                    BufferedImage img = ImageIO.read(inputStream);
+                    Assertions.assertThat(img.getHeight()).isEqualTo(1467);
+                    Assertions.assertThat(img.getWidth()).isEqualTo(2200);
+                }
+            }
+        }
+    }
+    @Test
+    void zipParsingCsvTest() throws Exception {
+        ZipFile zf = new ZipFile(new File("src/test/resources/CSV.zip"));
+        try (ZipInputStream is = new ZipInputStream(Objects.requireNonNull(classLoader.getResourceAsStream("CSV.zip")))) {
+            ZipEntry entry;
+            while ((entry = is.getNextEntry()) != null) {
+                org.assertj.core.api.Assertions.assertThat(entry.getName()).isEqualTo("addresses.csv");
+                try (InputStream inputStream = zf.getInputStream(entry)) {
+                    try (CSVReader reader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                        List<String[]> content = reader.readAll();
+                        Assertions.assertThat(content).contains(
+                                new String[]{"John","Doe","120 jefferson st.","Riverside", " NJ", " 08075"}
+                        );
+                    }
+                }
+            }
+        }
     }
 }
